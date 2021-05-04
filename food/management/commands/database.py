ea0799f5808111ac0623cc6eb152b5da50e8f2e6
JEREMY COMBES <68421626+Walrick@users.noreply.github.com>
2021-05-04 19:09:53 +0200

    build food app : add category update

--- food/management/commands/database.py
+++ food/management/commands/database.py
@@ -2,18 +2,20 @@
 # -*- coding: utf8 -*-
 
 from django.core.management.base import BaseCommand, CommandError
+from food.models import Category
+
 
 import food.openfoodfact_api as api_openfoodfact
 
 
 class Command(BaseCommand):
-    help = 'manage the database openfoodfact'
+    help = "manage the database openfoodfact"
 
     def add_arguments(self, parser):
 
         # Named (optional) arguments
         parser.add_argument(
-            '--category',
+            "--category",
         )
 
     def handle(self, *args, **options):
@@ -22,12 +24,8 @@ class Command(BaseCommand):
         if options["category"] == "up" or options["category"] == "update":
             api = api_openfoodfact.ApiOpenFoodFact()
             response = api.get_category()
-            print(response["count"])
-            count = 0
-            for i in response["tags"]:
-                if i["products"] > 10:
-                    print(i)
-                    count += 1
-            print(count)
 
-
+            for category in response["tags"]:
+                if category["products"] > 10:
+                    c = Category(category["name"])
+                    c.save()
