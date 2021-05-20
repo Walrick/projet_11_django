8df0401e80e4e66e1c9991dad7c39890b7760c6e
JEREMY COMBES <68421626+Walrick@users.noreply.github.com>
2021-05-20 16:55:58 +0200

    build views substitute.html : add pagination

--- food/views.py
+++ food/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from food.models import Products, Category
 
@@ -51,15 +52,43 @@ def product(request, id):
 
 def substitute(request, id):
     template = loader.get_template("food/substitute.html")
+    list_nutri = {"a" : 1,
+                  "b" : 2,
+                  "c" : 3,
+                  "d" : 4,
+                  "e" : 5,
+                  "Non applicable" : 6}
     data = {}
     try:
         product = Products.objects.get(id=id)
     except:
         product = None
     data["product"] = product
-    cat = product.category.all()
+    num_nutri_prod = list_nutri[product.nutrition_grade_fr]
+    list_cat = product.category.all()
+    list_product = []
+    for cat in list_cat:
+        try :
+            list_p = Products.objects.filter(category__products__id=cat.pk)
+            for p in list_p:
+                num_prod = list_nutri[p.nutrition_grade_fr]
+                if num_prod <= num_nutri_prod:
+                    list_product.append(p)
+        except:
+            p = None
 
-    print(cat[0].name)
+    paginator = Paginator(list_product, 21)
+    page = request.GET.get('page')
+    try:
+        result = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        result = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        result = paginator.page(paginator.num_pages)
+
+    data["result"] = result
 
     return HttpResponse(template.render(data, request=request))
 
