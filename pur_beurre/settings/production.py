cbd3b3e33bb475aef1cefb8554860b4306cf96b5
JEREMY COMBES <68421626+Walrick@users.noreply.github.com>
2021-05-31 16:51:34 +0200

    fix DATABASE_URL and STATICFILES for production

--- pur_beurre/settings/production.py
+++ pur_beurre/settings/production.py
@@ -10,4 +10,18 @@ ALLOWED_HOSTS = ["pur-beurre-1.herokuapp.com"]
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {"default": {dj_database_url.config(conn_max_age=500)}}
+DATABASES = {"default": {dj_database_url.config(default=os.environ['DATABASE_URL'])}}
+
+# Static files settings
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
