394e1bb68dfe3cf1fbf1611423557c24fa31d908
JEREMY COMBES <68421626+Walrick@users.noreply.github.com>
2021-06-01 19:14:02 +0200

    fix env_local_setting for production

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ def main():
     """Run administrative tasks."""
 
     # disable if production
-    env_setting()
+    # env_setting()
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pur_beurre.settings.production')
     try:
